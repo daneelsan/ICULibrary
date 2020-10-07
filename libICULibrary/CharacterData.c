4734cea5bf6df1a329ffa3368240096813fe6e54
Daniel Sanchez <dansando@protonmail.com>
2020-10-07 15:31:39 -0500

    Renaming libCharacterData C functions.

--- libICULibrary/CharacterData.c
+++ libICULibrary/CharacterData.c
@@ -5,13 +5,36 @@
 //#include <unicode/unistr.h>
 #include <unicode/uversion.h>
 
+// Gets the Unicode version information.
+DLLEXPORT int getUnicodeVersion(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
+{
+	UVersionInfo versionArray; // array of 4 uint8_t
+	MTensor T0;
+	mint i, dims[1];
+	int err = LIBRARY_NO_ERROR;
+
+	if (argc != 0) {
+		return LIBRARY_FUNCTION_ERROR;
+	}
+
+	u_getUnicodeVersion(versionArray);
+
+	dims[0] = U_MAX_VERSION_LENGTH;
+	err = libData->MTensor_new(MType_Integer, 1, dims, &T0);
+	for (i = 1; i <= U_MAX_VERSION_LENGTH && !err; ++i) {
+		err = libData->MTensor_setInteger(T0, &i, versionArray[i - 1]);
+	}
+	MArgument_setMTensor(res, T0);
+	return err;
+}
+
 #define MAX_ICUNAME_LEN 512
 static char icuNameBuff[MAX_ICUNAME_LEN + 1] = "\0";
 
 // Retrieve the name of a Unicode character.
-DLLEXPORT int icuCharacterName(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
+DLLEXPORT int characterName(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	mint nameLen;
 	UChar32 code;
@@ -43,9 +66,8 @@ DLLEXPORT int icuCharacterName(
 }
 
 // Get the "age" of the code point.
-DLLEXPORT int icuCharacter_charAge(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
+DLLEXPORT int characterAge(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	UChar32 c;
 	UVersionInfo versionArray; // array of 4 uint8_t
@@ -70,9 +92,8 @@ DLLEXPORT int icuCharacter_charAge(
 }
 
 // Get the numeric value for a Unicode code point as defined in the Unicode Character Database.
-DLLEXPORT int icuCharacter_getNumericValue(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
+DLLEXPORT int characterNumericValue(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	UChar32 c;
 	double val;
@@ -88,57 +109,9 @@ DLLEXPORT int icuCharacter_getNumericValue(
 	return LIBRARY_NO_ERROR;
 }
 
-// Gets the Unicode version information.
-DLLEXPORT int icuUnicodeVersion(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
-{
-	UVersionInfo versionArray; // array of 4 uint8_t
-	MTensor T0;
-	mint i, dims[1];
-	int err = LIBRARY_NO_ERROR;
-
-	if (argc != 0) {
-		return LIBRARY_FUNCTION_ERROR;
-	}
-
-	u_getUnicodeVersion(versionArray);
-
-	dims[0] = U_MAX_VERSION_LENGTH;
-	err = libData->MTensor_new(MType_Integer, 1, dims, &T0);
-	for (i = 1; i <= U_MAX_VERSION_LENGTH && !err; ++i) {
-		err = libData->MTensor_setInteger(T0, &i, versionArray[i - 1]);
-	}
-	MArgument_setMTensor(res, T0);
-	return err;
-}
-
 // Check a binary Unicode property for a code point.
-DLLEXPORT int icuCharacterBinaryProperty(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
-{
-	UChar32 c;
-	UProperty which;
-	UBool test;
-
-	if (argc != 2) {
-		return LIBRARY_FUNCTION_ERROR;
-	}
-	c = MArgument_getInteger(args[0]);
-	which = MArgument_getInteger(args[1]);
-
-	// Returns FALSE even if code point does not have data for the property.
-	test = u_hasBinaryProperty(c, which);
-
-	MArgument_setBoolean(res, test);
-	return LIBRARY_NO_ERROR;
-}
-
-// Check a binary Unicode property for a code point.
-DLLEXPORT int icuCharacterBoolProperty(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
+DLLEXPORT int characterBinaryProperty(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	UChar32 c;
 	UProperty which;
@@ -158,9 +131,8 @@ DLLEXPORT int icuCharacterBoolProperty(
 }
 
 // Get the property value for an enumerated or integer Unicode property for a code point.
-DLLEXPORT int icuCharacterIntProperty(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
+DLLEXPORT int characterIntPropertyValue(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	UChar32 c;
 	UProperty which;
@@ -178,9 +150,8 @@ DLLEXPORT int icuCharacterIntProperty(
 	return LIBRARY_NO_ERROR;
 }
 
-DLLEXPORT int icuCharacterIntNameProperty(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
+DLLEXPORT int characterIntPropertyValueName(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	UChar32 c;
 	UProperty prop;
@@ -206,9 +177,8 @@ DLLEXPORT int icuCharacterIntNameProperty(
 	return LIBRARY_NO_ERROR;
 }
 
-DLLEXPORT int icuCharacterCharProperty(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
+DLLEXPORT int characterCharProperty(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	UChar32 c;
 	UProperty prop;
@@ -250,8 +220,7 @@ DLLEXPORT int icuCharacterCharProperty(
 
 /*
 DLLEXPORT int icuCharacterJavaTestProperty(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	UChar32 c;
 	mint prop;
@@ -294,9 +263,8 @@ static JavaTest JavaTestFunctions[15] = {
 	u_isMirrored, u_istitle, u_isupper
 };
 
-DLLEXPORT int icuCharacterJavaTestProperty(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
+DLLEXPORT int characterJavaTestProperty(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	UChar32 c;
 	mint prop;
@@ -328,9 +296,8 @@ static CPOSIXTest CPOSIXTestFunctions[12] = {
 	u_isspace, u_isupper, u_isxdigit
 };
 
-DLLEXPORT int icuCharacterCPOSIXTestProperty(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
+DLLEXPORT int characterCPOSIXTestProperty(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	UChar32 c;
 	mint prop;
@@ -352,9 +319,8 @@ DLLEXPORT int icuCharacterCPOSIXTestProperty(
 	return LIBRARY_NO_ERROR;
 }
 
-DLLEXPORT int icuCharacterICUWhitespaceTestProperty(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
+DLLEXPORT int characterWhitespaceTestProperty(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	UChar32 c;
 	mint prop;
@@ -392,9 +358,8 @@ static ICUTest ICUTestFunctions[28] = {
 	u_isUUppercase, u_isUWhiteSpace, u_isWhitespace, u_isxdigit
 };
 
-DLLEXPORT int icuCharacterICUTestProperty(
-		WolframLibraryData libData, mint argc,
-		MArgument *args, MArgument res)
+DLLEXPORT int characterICUTestProperty(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	UChar32 c;
 	mint prop;
--- libICULibrary/CharacterData.h
+++ libICULibrary/CharacterData.h
@@ -3,31 +3,41 @@
 
 #include "WolframLibrary.h"
 
-DLLEXPORT int icuCharacterName(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
+DLLEXPORT int getUnicodeVersion(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
 
-DLLEXPORT int icuCharacter_charAge(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
+DLLEXPORT int characterName(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
 
-DLLEXPORT int icuCharacter_getNumericValue(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
+DLLEXPORT int characterAge(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
 
-DLLEXPORT int icuUnicodeVersion(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
+DLLEXPORT int characterNumericValue(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
 
-DLLEXPORT int icuCharacterBinaryProperty(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
+DLLEXPORT int characterBinaryProperty(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
 
-DLLEXPORT int icuCharacterBoolProperty(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
+DLLEXPORT int characterIntPropertyValue(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
 
-DLLEXPORT int icuCharacterIntProperty(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
+DLLEXPORT int characterIntPropertyValueName(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
 
-DLLEXPORT int icuCharacterIntNameProperty(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
+DLLEXPORT int characterCharProperty(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
 
-DLLEXPORT int icuCharacterCharProperty(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
+DLLEXPORT int characterJavaTestProperty(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
 
-DLLEXPORT int icuCharacterJavaTestProperty(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
+DLLEXPORT int characterCPOSIXTestProperty(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
 
-DLLEXPORT int icuCharacterCPOSIXTestProperty(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
+DLLEXPORT int characterWhitespaceTestProperty(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
 
-DLLEXPORT int icuCharacterICUWhitespaceTestProperty(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
-
-DLLEXPORT int icuCharacterICUTestProperty(WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
+DLLEXPORT int characterICUTestProperty(
+	WolframLibraryData libData, mint argc, MArgument *args, MArgument res);
 
 
 DLLEXPORT void WolframLibrary_uninitialize(WolframLibraryData libData);
