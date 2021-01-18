5c4d98b544d920dc9b75792b4df1e8aec06040d6
Daniel Sanchez <dansando@protonmail.com>
2021-01-17 23:59:34 -0500

    Improving .circleci scripts.

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -eo pipefail
 
 rm -f exit_status.txt
-STATUS_FILE=1 ./test.wls
-[[ -f exit_status.txt && $(< exit_status.txt) == "0" ]]
+STATUS_FILE=1 ./test.wls -lip "$@"
+[[ -f exit_status.txt && $(<exit_status.txt) == "0" ]]
